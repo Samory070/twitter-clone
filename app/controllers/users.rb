d6cf7bb73906e7a3676a38fd1022feb053072ad0
Amber Maillard <amber.maillard23@gmail.com>
2017-10-09 10:19:39 -0400

    Add and update user controller with new user creation routes. Add helper methods to session. Add new user view

---
+++ app/controllers/users.rb
@@ -0,0 +1,18 @@
+get '/users/new' do
+  @user = User.new
+  erb :'users/new'
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect "/users/login"
+  else
+    erb :"users/new"
+  end
+end
+
+get '/login' do
+  erb :'/users /login'
+end
