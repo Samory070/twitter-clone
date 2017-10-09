e44b57959e21a85799c6e7330bf75c45e2f79d44
Amber Maillard <amber.maillard23@gmail.com>
2017-10-09 09:59:02 -0400

    Add password, password hash, and user authenticate methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,28 @@ class User < ApplicationRecord
   has_many :tweets
   has_many :followers, foreign_key: "follower_id"
   has_many :followed_users, through: :followers, source: :followed
+
+#creating a user password
+    def password
+    @password ||= Password.new(password_hash)
+  end
+
+#creating the password hash
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+#authenticating a user
+  def self.authenticate(email, password)
+    user = User.find_by(email: email)
+    return nil unless user
+
+    if user.password == password
+      return user
+    else
+      return nil
+    end
+  end
+
 end
