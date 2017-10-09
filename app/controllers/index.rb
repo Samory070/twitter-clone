4b02ac3d0d4d6fddbdf572eca124e2c7ee24a256
Thierry Moudiki <email": null>
2017-10-09 16:30:37 -0400

    work on tweets

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,37 +1,6 @@
-get '/' do
+get '/tweets/index' do
   # Look in app/views/index.erb
-  erb :index
+  @tweets = Tweet.all
+  erb :'/tweets/index'
 end
 
-post '/users/signup' do
-  puts params
-  @user = User.new(:name => params[:name], :username => params[:username],:email => params[:email], :password => params[:password])
-  if @user.save
-    erb :'/users/new'
-  else
-    "Cannot sign up"
-  end
-end
-
-get '/users/new' do
-  erb :'/users/new'
-end
-
-get '/users/login' do
-  erb :'/users/login'
-end
-
-post '/users/login' do
-  @user = User.find_by(:username => params[:username])
-  if @user && @user.authenticate(params[:password])
-    session[:user_id] = @user.id
-    erb :'users/login'
-  else
-    "Cannot log in"
-  end
-end
-
-get '/users/logout' do
-  session[:user_id] = nil
-  redirect "/"
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@ User.delete_all
 Tweet.delete_all
 Follow.delete_all
 
-number_users = 25
+number_users = 50
+half_nb_users = number_users/2
 
 number_users.times do
   user_name = Faker::Internet.user_name
@@ -12,6 +13,11 @@ number_users.times do
                       password: Faker::Internet.password(10, 15))
   tweet = Tweet.create!(content: Faker::Twitter.status[:text],
     user_id: rand(1..number_users))
+
+
+  id1 = rand(1..half_nb_users)
+  id2 = rand((half_nb_users + 1)..number_users)
+  follow = Follow.create!(user_id1: id1, user_id2: (id1 + rand(1..id1)))
 
 end
 
