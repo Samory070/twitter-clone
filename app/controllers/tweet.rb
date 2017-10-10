f3119789fb1a1323c3bc40382baff066844ee722
Thierry Moudiki <email": null>
2017-10-10 19:17:21 -0400

    create new tweet by protecting the session

--- app/controllers/tweet.rb
+++ app/controllers/tweet.rb
@@ -4,3 +4,18 @@ get '/tweets/index' do
   erb :'/tweets/index'
 end
 
+get '/tweets/new' do
+  @tweets = Tweet.all
+  erb :'/tweets/new'
+end
+
+post '/tweets/new' do
+  puts params
+  # create new tweet by protecting the session
+  @tweet = Tweet.create!(:content => params[:content], :user_id => session[:user_id])
+  if @tweet.save
+    redirect '/tweets/index'
+  else
+    "User not created"
+  end
+end
